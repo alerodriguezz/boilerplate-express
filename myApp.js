c3d3a3645e392fe14a351dba2aa381cd8b2fc3ea
arod <>
2021-05-15 21:26:52 +0000

    added middleware

--- myApp.js
+++ myApp.js
@@ -2,21 +2,22 @@ var express = require('express');
 var app = express();
 
 
+console.log("Hello World");
 
+absolutePath = __dirname + "/views/index.html"
 
+//mount middlware
+app.use("/public", express.static(__dirname + "/public"));
 
+/*
+app.get("/",function(req, res) {
+  res.send('Hello Express');
+});
+*/
 
-
-
-
-
-
-
-
-
-
-
-
+app.get("/",function(req, res) {
+  res.sendFile(absolutePath);
+});
 
 
 
