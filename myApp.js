ec0d000fd0a28bb37568da8e00ae17d8c4dbaeaf
arod <>
2021-05-20 06:27:31 +0000

    Set up post endpoint. Finished lab.

--- myApp.js
+++ myApp.js
@@ -1,3 +1,6 @@
+
+
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
@@ -7,6 +10,9 @@ console.log("Hello World");
 absolutePath = __dirname + "/views/index.html"
 
 //mount middlware
+      //Use body-parser to Parse POST Requests
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 app.use("/public", express.static(__dirname + "/public"));
 
 app.use(function middleware(req, res, next) {
@@ -52,8 +58,7 @@ app.get('/now',
     res.json({"time":req.string});
   }
 );
- //Get Route Parameter Input from the Client
-
+ //Get ROUTE Parameter Input from the Client
 app.get('/:word/echo',
    (req, res) => {
     // accessing the newly added property
@@ -62,15 +67,29 @@ app.get('/:word/echo',
   }
 );
 
+ //Get QUERY Parameter Input from the Client e.g ?first=firstname&last=lastname
+app.get('/name',
+   (req, res) => {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+   
+   /* this is also valid...
+  var { first: firstName, last: lastName } = req.query;
+  */
+    res.json({name: `${firstName} ${lastName}` });
+  }
+);
 
+//Get Data from POST Requests 
 
+app.post('/name',
+(req,res) => {
+var firstName = req.body.first;
+var lastName = req.body.last;
 
-
-
-
-
-
-
+res.json({name: `${firstName} ${lastName}` });
+}
+);
 
 
  module.exports = app;
